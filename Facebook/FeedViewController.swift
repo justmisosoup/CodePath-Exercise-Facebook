687a9d6f75c791aaeda451a796e878beeb7e388c
Sara Menefee <sara@menef.ee>
2014-09-15 21:39:14 -0700

    Create Facebook Project

---
+++ Facebook/FeedViewController.swift
@@ -0,0 +1,37 @@
+//
+//  FeedViewController.swift
+//  Facebook
+//
+//  Created by Sara Menefee on 9/15/14.
+//  Copyright (c) 2014 Sara Menefee. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    @IBOutlet weak var feedScrollView: UIScrollView!
+    override func viewDidLoad() {
+        super.viewDidLoad(feedScrollView.contentSize = CGSize(width: 320, height: 1000))
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+   
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Facebook/ThrillistDetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ThrillistDetailViewController.swift
+//  Facebook
+//
+//  Created by Sara Menefee on 9/15/14.
+//  Copyright (c) 2014 Sara Menefee. All rights reserved.
+//
+
+import UIKit
+
+class ThrillistDetailViewController: UIViewController {
+
+    @IBOutlet weak var thrillistDetailScrollView: UIScrollView!
+    override func viewDidLoad() {
+        super.viewDidLoad(thrillistDetailScrollView.contentSize = CGSize(width: 320, height: 1000))
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func clickBackToFeed(sender: AnyObject) {
+        navigationController?.popViewControllerAnimated(true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
